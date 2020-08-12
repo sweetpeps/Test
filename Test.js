ee4d579f658aef4678488509bf5aee8d25140c1d
sweetpeps <39718933+sweetpeps@users.noreply.github.com>
2020-08-12 15:28:46 +1000

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,8 @@
+var button = document.createElement("button");
+button.innerHTML = "";
+
+button.addEventListener ("click", function() {
+  window.open("https://www.google.com")
+});
+
+button.click()
